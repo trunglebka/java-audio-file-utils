43d3cbc91a4c679a7f7254d82380612f4ff334ef
trunglebka <cauvongcuoitroi@gmail.com>
2021-09-10 17:06:58 +0700

    Add changing audio format test

--- src/test/java/vtcc/voice/core/audiofileconverter/ffmpegbased/FfmpegAudioFileConverterTest.java
+++ src/test/java/vtcc/voice/core/audiofileconverter/ffmpegbased/FfmpegAudioFileConverterTest.java
@@ -3,6 +3,8 @@ package vtcc.voice.core.audiofileconverter.ffmpegbased;
 import vtcc.voice.core.audiofile.AudioFile;
 import vtcc.voice.core.audiofile.AudioFileType;
 import vtcc.voice.core.audiofile.AudioFormat;
+import vtcc.voice.core.audiofile.metadatareader.AudioFileMetadataReader;
+import vtcc.voice.core.audiofile.metadatareader.FfmpegAudioFileMetadataReader;
 import vtcc.voice.core.audiofileconverter.AudioFileConverter;
 
 import java.io.IOException;
@@ -10,6 +12,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 /**
  * Created by Trung Le. <br>
  * Email: trungle.bka@gmail.com <br>
@@ -19,6 +22,7 @@ import java.nio.file.Paths;
 class FfmpegAudioFileConverterTest {
   private Path tmpTestDir;
   private Path ffmpegPath = Paths.get("/usr/bin/ffmpeg");
+  private AudioFileMetadataReader audioFileMetadataReader;
 
   public FfmpegAudioFileConverterTest() throws IOException {
     String envTmpdir = System.getProperty("java.io.tmpdir");
@@ -30,6 +34,7 @@ class FfmpegAudioFileConverterTest {
     if (Files.notExists(tmpTestDir)) {
       Files.createDirectories(tmpTestDir);
     }
+    audioFileMetadataReader = FfmpegAudioFileMetadataReader.fromDefaultFFmpegProbe();
   }
 
   @org.junit.jupiter.api.Test
@@ -59,5 +64,28 @@ class FfmpegAudioFileConverterTest {
     AudioFileConverter audioFileConverter =
         FfmpegWrappedAudioFileConverter.builder().ffmpegExecutable(ffmpegPath).build();
     audioFileConverter.convertFile(inFile, outFile);
+  }
+
+  @org.junit.jupiter.api.Test
+  void convertFileM4aToWavChangeFormat() throws Exception {
+    Path wavFile = Paths.get("src/test/resources/audio/8k.m4a");
+    Path m4aFile = tmpTestDir.resolve("wav-test-out.wav");
+    AudioFile inFile = FfmpegInputAudioFile.fromLocalFile(wavFile);
+    int targetChannel = 2;
+    int targetRate = 4000;
+    AudioFormat targetFormat =
+        AudioFormat.builder().numChannels(targetChannel).sampleRate(targetRate).build();
+    AudioFile outFile =
+        FfmpegOutputAudioFile.builder()
+            .filePath(m4aFile)
+            .audioFormat(targetFormat)
+            .fileType(AudioFileType.WAV)
+            .build();
+    AudioFileConverter audioFileConverter =
+        FfmpegWrappedAudioFileConverter.builder().ffmpegExecutable(ffmpegPath).build();
+    audioFileConverter.convertFile(inFile, outFile);
+    AudioFile metadataLoadedAudioFile = audioFileMetadataReader.readFromFile(outFile.getFilePath());
+    assertEquals(targetChannel, metadataLoadedAudioFile.getAudioFormat().getNumChannels());
+    assertEquals(targetRate, metadataLoadedAudioFile.getAudioFormat().getSampleRate());
   }
 }
